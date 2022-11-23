6c4b04caf8ab4c82a8d79d0632c5503246f1504d
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-23 15:19:04 +0000

    Java Scanner

--- src/main/java/org/playground/Main.java
+++ src/main/java/org/playground/Main.java
@@ -1,9 +1,19 @@
 package org.playground;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int pies = 10, people = 4;
-        double piesPerPerson = (double) pies / people;
-        System.out.println(piesPerPerson);
+        String input;
+        char answer;
+
+        // Create a Scanner object for keyboard input.
+        Scanner keyboard = new Scanner(System.in);
+        // Ask the user a question.
+        System.out.print("Are you having fun? (Y=yes, N=no) ");
+        input = keyboard.nextLine(); // Get a line of input.
+        answer = input.charAt(0); // Get the first character.
+        System.out.println(answer);
+
     }
 }

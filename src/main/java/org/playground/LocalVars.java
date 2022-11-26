d91762c038cbdfbc6358a625df76ee1927d3784a
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-26 20:08:06 +0000

    Methods

---
+++ src/main/java/org/playground/LocalVars.java
@@ -0,0 +1,28 @@
+package org.playground;
+
+public class LocalVars {
+    public static void main(String[] args) {
+        texas();
+        california();
+    }
+
+    /**
+     * The california method has a local variable named birds.
+     */
+    public static void california() {
+        int birds = 3500;
+        System.out.println("In california there are " + birds + " birds.");
+    }
+
+    /*
+        The texas method has a local variable named birds.
+     */
+    public static void texas() {
+        int birds = 5000;
+        System.out.println("In texas there are " + birds + " birds.");
+    }
+
+
+
+
+}

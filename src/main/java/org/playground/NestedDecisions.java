09c8a683278927a0ebbef428f748827858cd2ee3
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-03 21:39:06 +0000

    Nested if

---
+++ src/main/java/org/playground/NestedDecisions.java
@@ -0,0 +1,30 @@
+package org.playground;
+
+import javax.swing.*;
+
+/**
+ * This program asks the user to enter a numeric
+ * test score and displays a letter grade for the
+ * score. The program uses nested decision structure
+ * to determine the score.
+ */
+
+public class NestedDecisions {
+    public static void main(String[] args) {
+        int testScore;
+        String input;
+
+        // Get the numeric test score.
+        input = JOptionPane.showInputDialog("Enter your numeric test score and" +
+                " I will tell you the grade");
+        testScore = Integer.parseInt(input);
+
+        // Display the grade
+        if (testScore < 60) JOptionPane.showMessageDialog(null, "Your grade is F");
+        else if (testScore < 70) JOptionPane.showMessageDialog(null, "Your grade is D");
+        else if (testScore < 80) JOptionPane.showMessageDialog(null, "Your grade is C");
+        else if (testScore < 90) JOptionPane.showMessageDialog(null, "Your grade is B");
+        else JOptionPane.showMessageDialog(null, "Your grade is A");
+        System.exit(0);
+    }
+}

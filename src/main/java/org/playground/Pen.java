dfdd38c73025ab90c2de89394cbf8692dd874af8
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-03 11:05:57 +0000

    simple method

--- src/main/java/org/playground/Pen.java
+++
@@ -1,4 +0,0 @@
-package org.playground;
-
-public class Pen {
-}
--- src/main/java/org/playground/SimpleMethod.java
+++ src/main/java/org/playground/SimpleMethod.java
@@ -1,6 +1,9 @@
 package org.playground;
 
-public class Main {
+/**
+ * This program defines and calls a simple method.
+ */
+public class SimpleMethod {
     public static void main(String[] args) {
         System.out.println("Hello from main()");
         displayMessage();

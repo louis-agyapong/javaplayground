ffb07b36773f8352fda7b0bd63e7de062175afac
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-03 08:33:48 +0000

    Code refactor

--- src/main/java/org/playground/Main.java
+++ src/main/java/org/playground/Main.java
@@ -1,18 +1,7 @@
 package org.playground;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        String input;
-        char answer;
-
-        // Create a Scanner object for keyboard input.
-        Scanner keyboard = new Scanner(System.in);
-        // Ask the user a question.
-        System.out.print("Are you having fun? (Y=yes, N=no) ");
-        input = keyboard.nextLine(); // Get a line of input.
-        answer = input.charAt(0); // Get the first character.
-        System.out.println(answer);
+        System.out.println("Main");
     }
 }
---
+++ src/main/java/org/playground/Pen.java
@@ -0,0 +1,4 @@
+package org.playground;
+
+public class Pen {
+}

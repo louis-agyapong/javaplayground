13ede0dc93322dd4ef0e6c2aa8710552d7480bcf
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-07 07:17:27 +0000

    Simple methods

--- src/main/java/org/playground/LoopCall.java
+++ src/main/java/org/playground/LoopCall.java
@@ -3,6 +3,12 @@ package org.playground;
 public class LoopCall {
     public static void main(String[] args) {
         System.out.println("Hello from main()");
+        for (int i = 0; i < 5; i++)
+            displayMethod();
+        System.out.println("Back to main()");
+    }
 
+    public static void displayMethod() {
+        System.out.println("Hello from displayMethod()");
     }
 }

a31d246608a23a76b4d8660244597eb3bfc8d311
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-07 21:16:44 +0000

    Overloading methods and constructors

---
+++ src/main/java/org/playground/BankAccount.java
@@ -0,0 +1,50 @@
+package org.playground;
+
+/**
+ * The bank account class simulates a bank account.
+ */
+
+public class BankAccount {
+    // Account balance.
+    private double balance;
+
+    /**
+     * This constructor sets the starting balance
+     * at 0.0.
+     */
+    public BankAccount() {
+        this.balance = 0.0;
+    }
+
+    public BankAccount(double startingBalance) {
+        this.balance = startingBalance;
+    }
+
+    public BankAccount(String str) {
+        this.balance = Double.parseDouble(str);
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void deposit(String str) {
+        this.balance += Double.parseDouble(str);
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void withdraw(String str) {
+        this.balance -= Double.parseDouble(str);
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setBalance(String str) {
+        this.balance = Double.parseDouble(str);
+    }
+}

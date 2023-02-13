1a2752e445ccc13394b1addbebbc9727493b3309
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-02-13 04:12:24 +0000

    Decision Statement (If-else Statement)

---
+++ src/main/java/org/playground/Division.java
@@ -0,0 +1,35 @@
+package org.playground;
+
+import java.util.Scanner;
+
+/**
+ * This program demonstrates the if-else statement.
+ */
+
+public class Division {
+    public static void main(String[] args) {
+        double number1, number2; // Division operands
+        double quotient; // Result of division
+
+        // Create a scanner object for keyboard input.
+        Scanner keyboard = new Scanner(System.in);
+
+        // Get the first number.
+        System.out.print("Enter a number: ");
+        number1 = keyboard.nextDouble();
+
+        // Get the second number.
+        System.out.print("Enter another number: ");
+        number2 = keyboard.nextDouble();
+
+        if (number2 == 0) {
+            System.out.println("Division by zero is not possible.");
+            System.out.println("Please run the program again and ");
+            System.out.println("enter a number other than zero.");
+        } else {
+            quotient = number1 / number2;
+            System.out.print("The quotient of " + number1 + " divided by " + number2
+                    + " is " + quotient);
+        }
+    }
+}

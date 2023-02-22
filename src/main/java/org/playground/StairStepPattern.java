2d0a23ce4e7db8c559ca37847afa0df2992a914e
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-02-22 12:28:39 +0000

    Loops ([Nested Loop])

---
+++ src/main/java/org/playground/StairStepPattern.java
@@ -0,0 +1,17 @@
+package org.playground;
+
+/**
+ * This program displays a stair step pattern
+ */
+
+public class StairStepPattern {
+    public static void main(String[] args) {
+        final int NUM_STEPS = 6;
+        for (int r = 0; r < NUM_STEPS; r++) {
+            for (int c = 0; c < r; c++) {
+                System.out.print(" ");
+            }
+            System.out.println("#");
+        }
+    }
+}
